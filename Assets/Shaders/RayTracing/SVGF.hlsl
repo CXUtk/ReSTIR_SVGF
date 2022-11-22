25fe12762dde0c60d6a86b89aa8a8dc3c6eb0bc0
CXUtk <2237367067@qq.com>
2022-11-22 08:49:59 -0800

    Bug fix

--- Assets/Shaders/RayTracing/SVGF.hlsl
+++ Assets/Shaders/RayTracing/SVGF.hlsl
@@ -195,7 +195,7 @@ float4 main_filter (v2f V2F) : SV_TARGET
 
 float4 variance_estimation (v2f V2F) : SV_TARGET
 {
-    int2 imageCoord = round(V2F.uv / _invScreenSize.xy);
+    int2 imageCoord = floor(V2F.uv / _invScreenSize.xy);
     int bufferId = imageCoord.y * (int)_invScreenSize.z + imageCoord.x;
     
     float3 N = _normalM.SampleLevel(my_point_clamp_sampler, V2F.uv, 0).xyz;
